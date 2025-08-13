e097e30d3b786e7eecb62cf8f405a75245bfaffa
anononononim147 <anononononim147@gmail.com>
2025-08-13 23:03:35 +0200

    files uploaded

---
+++ lib/smplx/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Anonymous is
+# holder of all proprietary rights on this computer program.
+# You can only use this computer program if you have closed
+# a license agreement with Anonymous or you get the right to use the computer
+# program from someone who is authorized to grant you that right.
+# Any use of the computer program without a valid license is prohibited and
+# liable to prosecution.
+#
+# Anonymous. acting on behalf of its Anonymous. All rights reserved.
+#
+# Contact: anonymous
+
+from .body_models import (
+    FLAME,
+    MANO,
+    SMPL,
+    SMPLH,
+    SMPLX,
+    FLAMELayer,
+    MANOLayer,
+    SMPLHLayer,
+    SMPLLayer,
+    SMPLXLayer,
+    build_layer,
+    create,
+)
